826a7779a10ab2632a38a5b3ff7e6072feb73984
prateekkaushal7886 <prateekkaushal7886@gmail.com>
2017-08-17 21:19:25 +0530

    sdcc

---
+++ body.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<style type="text/css">
+
+.col-sm-6
+{
+	box-shadow: 3px 3px 6px 6px rgba(0,0,0,.4);
+	background-image: url('2.4.jpg');
+	
+}
+
+
+
+.col-sm-6:hover
+{
+	box-shadow: 5px 5px 10px 10px rgba(0,0,255,.3);
+	background-image: url('2.2.jpg');
+}
+p
+{
+	font-size: 130%;
+	font-weight: bold;
+}
+
+
+</style>
+ 
+
+
+ <body>
+  	<br /><br />
+<div class="col-sm-3"></div>
+<div class="col-sm-6">
+	
+<p>
+	The Students' Alumni Cell, IIT Kharagpur is a voluntary student body working under the aegis 
+                      of the Dean of Alumni Affairs and International Relations. It strives to bridge the gap between 
+                      IIT Kharagpur and its esteemed alumni community.<br>
+                            <br>
+                       The Students' Alumni Cell continuously endeavours to strengthen the relations of 
+                       the alumni of this Institute and their alma mater. 
+                       It has been instrumental in organising the Annual Alumni Meet, Regional Student Alumni Meet, 
+                       regular guest lectures by distinguished alumni in the campus, publishing the annual 
+                       literary magazine (Yearnings of Yore) & the annual yearbook.  
+                       The Students' Alumni Cell also operates the Student - Alumni Mentorship Programme, My Imprint
+                      (Giving Back - Alumni contribution) and organises Alvida (the annual farewell dinner).
+</p>
+
+
+
+</div> 
+
+<div class="col-sm-3"></div>
+
+
+
+
+
+
+
+
+  </body>
+
+
+  https://codepen.io/nishant88/pen/yYyePL
+
+  https://codepen.io/IamManchanda/pen/zoaWdW
